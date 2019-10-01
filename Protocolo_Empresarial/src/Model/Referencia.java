2c957f1f0f230f21ba1bde066623e4c277603f37
reinaldo.nti <reinaldo.nti@hcf782.FAMEMA>
2019-10-01 09:49:06 -0300

    atributos e encapsulamentos

--- Protocolo_Empresarial/src/Model/Referencia.java
+++ Protocolo_Empresarial/src/Model/Referencia.java
@@ -10,5 +10,22 @@ package Model;
  * @author piga
  */
 public class Referencia {
-    
+    private String nome;
+    private String tipo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
