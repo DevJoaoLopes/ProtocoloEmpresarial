b22dbcb2284474cb26adb5c324755041b7b833fa
Joao <joaopedro_oliver@hotmail.com>
2019-10-25 18:12:22 -0300

    create page TipoReferencia

---
+++ Protocolo_Empresarial/src/Model/TipoReferencia.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author joaop
+ */
+public class TipoReferencia {
+    
+     private String descricao;
+    
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
